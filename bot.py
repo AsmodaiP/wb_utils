0483b3e761b68f5da4641c456dfd6ae7230d997d
Dmitriy Pereguda <asmoday.p@gmail.com>
2022-04-09 17:39:20 +0300

    init

---
+++ bot.py
@@ -0,0 +1,84 @@
+from cmath import inf
+from http.client import ImproperConnectionState
+import imp
+from logging import Filter
+from turtle import update
+from dotenv import load_dotenv
+import os
+
+import telegram
+from telegram import Bot, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import (CommandHandler, ConversationHandler, Filters,
+                          MessageHandler, Updater)
+import change_price
+
+
+MAIN_MENU = (
+    [KeyboardButton('Поменять цену')],
+)
+MAIN_MENU_MARKUP = ReplyKeyboardMarkup(
+    MAIN_MENU,
+    resize_keyboard=True,
+    one_time_keyboard=False)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv()
+
+TOKEN = os.environ['TOKEN']
+bot: Bot = telegram.Bot(TOKEN)
+WHITELIST= os.environ['WHITELIST'].split(',')
+def start(bot: Bot, update):
+    bot.message.reply_text('sfsdf', reply_markup=MAIN_MENU_MARKUP)
+
+updater = Updater(token=TOKEN)
+
+
+start_handler = CommandHandler('start', start)
+updater.dispatcher.add_handler(start_handler)
+
+def get_article(bot, update):
+    if str(bot['message']['chat']['id']) not in WHITELIST:
+        bot.message.reply_text('Операция запрещена, для получения доступа обратитесь к @dmitriy_pereguda')
+        return ConversationHandler.END
+    bot.message.reply_text('Введите артикул')
+    return 'get_current_info'
+
+def get_current_info(bot, update):
+    try:
+        article = int(bot.message.text.strip())
+    except:
+        bot.message.reply_text('Артикул должен быть числом (внутренний артикул вб). Начните заново')
+        return ConversationHandler.END
+    info = change_price.get_info_current_price(article)
+    if info is None:
+        bot.message.reply_text('Артикул не найден')
+        return ConversationHandler.END
+    update.user_data['article'] = article
+    bot.message.reply_text(f'Текущая цена {info["price"]},\n Cкидка {info["discount"]} \n Промокод {info["promoCode"]}')
+    bot.message.reply_text('Введите новую цену')
+    return 'change_price'
+
+def change_price_by_bot(bot, update):
+    new_price = int(bot.message.text.strip())
+    article = update.user_data['article'] 
+    result = change_price.change_price(article, new_price)
+    bot.message.reply_text(result)
+    return ConversationHandler.END
+
+def cancel(bot, update):
+    bot.message.reply_text('Операция отменена')
+    return ConversationHandler.END
+
+change_price_handler = ConversationHandler(
+    entry_points=[MessageHandler(Filters.text(
+        ['Поменять цену']), get_article)],
+    states={
+        'get_current_info':[MessageHandler(Filters.text & ~Filters.command, get_current_info)],
+        'change_price': [MessageHandler(Filters.text & ~Filters.command, change_price_by_bot)],
+    },
+    fallbacks=[CommandHandler('cancel', cancel)]
+    )
+updater.dispatcher.add_handler(change_price_handler)
+updater.start_polling()
+
+
+
---
+++ change_price.py
@@ -0,0 +1,55 @@
+from cmath import inf
+import requests
+import json
+
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+CRED = json.load(open(os.path.join(BASE_DIR,'credentials.json'), 'rb'))
+
+def get_info_current_price(article):
+    for name in CRED.keys():
+        token = CRED[name]['token']
+
+        headers = {
+            'Authorization': token,
+        }
+        url = "https://suppliers-api.wildberries.ru/public/api/v1/info"
+        response = requests.get(url, headers=headers, params={'quantity': 0})
+        for info in response.json():
+            if info['nmId'] == article:
+                info['Цена после скидок'] = int(info['price']*(1-info['discount']*0.01- info['promoCode']*0.01))
+                return info
+
+
+
+def validate_new_price(current_price, new_price):
+    if current_price * 0.85 > new_price:
+        return False
+    return True
+
+def change_price(article, new_price):
+    url ="https://suppliers-api.wildberries.ru/public/api/v1/prices"
+
+    current_price_info = get_info_current_price(article)
+
+    if  not validate_new_price(current_price_info['price'], new_price):
+        return 'Несоответствие цены'
+    for name in CRED.keys():
+        token = CRED[name]['token']
+
+        headers = {
+            'Authorization': token,
+        }
+        json = [
+            {
+                "nmId": article,
+                "price": new_price
+            }
+        ]
+        response = requests.post(url, headers=headers, json=json)
+        if response.status_code == 200:
+            return 'Цена изменена'
+
+# print(get_info_current_price(67937389))
+# print(change_price(67937389, 3095))
