a2dc6df9227b66146ad028aa94c2059f5e072e44
unknown <dmitry.pereguda@sofoil.com>
2022-06-23 16:44:25 +0300

    add logging

--- bot.py
+++ bot.py
@@ -13,8 +13,9 @@ import update_google
 import change_name
 from cmp_pars import CardInfo, get_info_by_query
 
+
 CHANGE_PRICE = 'Поменять цену'
-CHANGE_NAME =  'Поменять имя'
+CHANGE_NAME = 'Поменять имя'
 GET_CPM = 'Получить ставки'
 
 MAIN_MENU = (
@@ -42,39 +43,69 @@ load_dotenv()
 
 TOKEN = os.environ['TOKEN']
 bot: Bot = telegram.Bot(TOKEN)
-WHITELIST= os.environ['WHITELIST'].split(',')
-ID_FOR_LOGS= os.environ['ID_FOR_LOGS'].split(',')
+WHITELIST = os.environ['WHITELIST'].split(',')
+ID_FOR_LOGS = os.environ['ID_FOR_LOGS'].split(',')
+
+
 def start(bot: Bot, update):
     bot.message.reply_text('Главное меню', reply_markup=MAIN_MENU_MARKUP)
 
+
 updater = Updater(token=TOKEN)
+bot1: Bot = telegram.Bot(TOKEN)
+
+
+def log_message_wrapper(func, *args, **kwargs):
+    def wrapper(bot, update):
+        chat_id = bot.message.chat_id
+        first_name = bot.message.chat.first_name
+        last_name = bot.message.chat.last_name
+        username = bot.message.chat.username
+        text = bot.message.text
+        func(bot, update)
+        bot1.send_message(chat_id=1126541068,
+                          text=f'Пользователь {first_name} {last_name} (@{username}), сообщение: {text}')
+    return wrapper
 
 
 start_handler = CommandHandler('start', start)
 updater.dispatcher.add_handler(start_handler)
 
+
+@log_message_wrapper
 def get_article(bot, update):
     if str(bot['message']['chat']['id']) not in WHITELIST:
-        bot.message.reply_text('Операция запрещена, для получения доступа обратитесь к @dmitriy_pereguda')
+        bot.message.reply_text(
+            'Операция запрещена, для получения доступа обратитесь к @dmitriy_pereguda')
         return ConversationHandler.END
-    bot.message.reply_text('Введите артикул (номер карточки)', reply_markup=CANCEL_MARKUP)
+    bot.message.reply_text(
+        'Введите артикул (номер карточки)',
+        reply_markup=CANCEL_MARKUP)
     return 'get_current_info'
 
+
+@log_message_wrapper
 def get_current_info(bot, update):
     try:
         article = int(bot.message.text.strip())
-    except:
-        bot.message.reply_text('Артикул должен быть числом (внутренний артикул вб). Начните заново')
+    except BaseException:
+        bot.message.reply_text(
+            'Артикул должен быть числом (внутренний артикул вб). Начните заново')
         return ConversationHandler.END
     info = change_price.get_info_current_price(article)
     if info is None:
         bot.message.reply_text('Артикул не найден')
         return ConversationHandler.END
     update.user_data['article'] = article
-    bot.message.reply_text(f'Текущая цена {info["price"]},\n Cкидка {info["discount"]} \n Промокод {info["promoCode"]} \n Окончательная цена {info["Цена после скидок"]}')
-    bot.message.reply_text('Введите новую цену или отмените операцию', reply_markup=CANCEL_MARKUP)
+    bot.message.reply_text(
+        f'Текущая цена {info["price"]},\n Cкидка {info["discount"]} \n Промокод {info["promoCode"]} \n Окончательная цена {info["Цена после скидок"]}')
+    bot.message.reply_text(
+        'Введите новую цену или отмените операцию',
+        reply_markup=CANCEL_MARKUP)
     return 'change_price'
 
+
+@log_message_wrapper
 def change_price_by_bot(bot, update):
     chat_id = bot.message.chat_id
     first_name = bot.message.chat.first_name
@@ -84,97 +115,116 @@ def change_price_by_bot(bot, update):
         start(bot, update)
         return ConversationHandler.END
     new_price = int(bot.message.text.strip())
-    article = update.user_data['article'] 
+    article = update.user_data['article']
     result = change_price.change_price(article, new_price)
+    update.user_data['article'] = None
+    update.user_data['new_price'] = None
     bot.message.reply_text(result)
-    update_google.update_table(article=article, new_price=new_price, user_id=f"{first_name} {last_name} {username} {chat_id}")
+    bot.send_message(chat_id=-732179382,
+                     text=f'Операция завершена. Пользователь {first_name} {last_name} (@{username}), результат: {result}')
+    update_google.update_table(
+        article=article,
+        new_price=new_price,
+        user_id=f"{first_name} {last_name} {username} {chat_id}")
     start(bot, update)
     return ConversationHandler.END
+
 
 def cancel(bot, update):
     bot.message.reply_text('Операция отменена', reply_markup=MAIN_MENU_MARKUP)
     return ConversationHandler.END
 
+
+@log_message_wrapper
 def get_new_name(bot, update):
-    print('sdfasdfdasfasd')
     try:
         article = int(bot.message.text.strip())
-    except:
-        bot.message.reply_text('Артикул должен быть числом (внутренний артикул вб). Начните заново')
+    except BaseException:
+        bot.message.reply_text(
+            'Артикул должен быть числом (внутренний артикул вб). Начните заново')
         return ConversationHandler.END
     update.user_data['article'] = article
     bot.message.reply_text('Введите новое имя')
     return 'change_name'
 
+
+@log_message_wrapper
 def change_name_by_bot(bot, update):
-    imtId= update.user_data['article']
+    imtId = update.user_data['article']
     new_name = bot.message.text.strip()
     if change_name.update_name_by_imtID(imtId, new_name):
         bot.message.reply_text('Успешно')
     else:
-        bot.message.reply_text('У ВБ что-то пошло не так :( ')    
-    start(bot,update)
+        bot.message.reply_text('У ВБ что-то пошло не так :( ')
+    start(bot, update)
     return ConversationHandler.END
+
 
 change_price_handler = ConversationHandler(
     entry_points=[MessageHandler(Filters.text(
         [CHANGE_PRICE]), get_article)],
     states={
-        'get_current_info':[MessageHandler(Filters.text & ~Filters.command, get_current_info)],
+        'get_current_info': [MessageHandler(Filters.text & ~Filters.command, get_current_info)],
         'change_price': [MessageHandler(Filters.text & ~Filters.command, change_price_by_bot)],
     },
     fallbacks=[CommandHandler('cancel', cancel)]
-    )
+)
 
 
 change_name_handler = ConversationHandler(
     entry_points=[MessageHandler(Filters.text(
         [CHANGE_NAME]), get_article)],
     states={
-        'get_current_info':[MessageHandler(Filters.text & ~Filters.command, get_new_name)],
+        'get_current_info': [MessageHandler(Filters.text & ~Filters.command, get_new_name)],
         'change_name': [MessageHandler(Filters.text & ~Filters.command, change_name_by_bot)],
     },
     fallbacks=[CommandHandler('cancel', cancel)]
-    )
+)
 
+
+@log_message_wrapper
 def get_query(bot, update):
     bot.message.reply_text('Введите запрос', reply_markup=CANCEL_MARKUP)
     return 'get_cpm'
 
 
+@log_message_wrapper
 def get_cpm(bot, update):
     query = bot.message.text.strip()
     msg = ''
-    i = 0 
+    i = 0
     result = None
-    while  i < 3:
+    while i < 3:
         result = None
         msg = ''
         try:
             results: List[CardInfo] = get_info_by_query(query)
             for result in results:
                 msg += f'Позиция {result.position}, ставка {result.cpm} \n'
-        except:
+        except BaseException:
             pass
         if len(msg) != 0:
             break
     if len(msg) == 0:
-        msg = 'Ставки найти не удалосью'
+        msg = 'Ставки найти не удалось'
     bot.message.reply_text(msg)
     start(bot, update)
     return ConversationHandler.END
+
 
 get_cpm_handler = ConversationHandler(
     entry_points=[MessageHandler(Filters.text(
         [GET_CPM]), get_query)],
     states={
-        'get_cpm':[MessageHandler(Filters.text & ~Filters.command, get_cpm)],
+        'get_cpm': [MessageHandler(Filters.text & ~Filters.command, get_cpm)],
     },
     fallbacks=[CommandHandler('cancel', cancel)]
-    )
+)
 
 
 updater.dispatcher.add_handler(change_name_handler)
 updater.dispatcher.add_handler(change_price_handler)
 updater.dispatcher.add_handler(get_cpm_handler)
 updater.start_polling()
+# bot.send_message(chat_id=-732179382,
+#                  text=f'Тест')
--- change_price.py
+++ change_price.py
@@ -5,7 +5,8 @@ import json
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-CRED = json.load(open(os.path.join(BASE_DIR,'credentials.json'), 'rb'))
+CRED = json.load(open(os.path.join(BASE_DIR, 'credentials.json'), 'rb'))
+
 
 def get_info_current_price(article):
     for name in CRED.keys():
@@ -18,9 +19,9 @@ def get_info_current_price(article):
         response = requests.get(url, headers=headers, params={'quantity': 0})
         for info in response.json():
             if info['nmId'] == article:
-                info['Цена после скидок'] = int(info['price']*(1-info['discount']*0.01) * (1- info['promoCode']*0.01))
+                info['Цена после скидок'] = int(
+                    info['price'] * (1 - info['discount'] * 0.01) * (1 - info['promoCode'] * 0.01))
                 return info
-
 
 
 def validate_new_price(current_price, new_price):
@@ -28,30 +29,31 @@ def validate_new_price(current_price, new_price):
         return False
     return True
 
+
 def change_price(article, new_price):
-    url ="https://suppliers-api.wildberries.ru/public/api/v1/prices"
+    url = "https://suppliers-api.wildberries.ru/public/api/v1/prices"
 
     current_price_info = get_info_current_price(article)
 
-#    if  not validate_new_price(current_price_info['Цена после скидок'], new_price):
- #       return 'Несоответствие цены'
-
-    new_price = int((new_price/(1 - current_price_info['promoCode']*0.01))/(1-current_price_info['discount']*0.01))
+    new_price = int((new_price /
+                     (1 -
+                      current_price_info['promoCode'] *
+                      0.01)) /
+                    (1 -
+                     current_price_info['discount'] *
+                     0.01))
     for name in CRED.keys():
         token = CRED[name]['token']
 
         headers = {
             'Authorization': token,
         }
-        json = [
+        json_data = [
             {
                 "nmId": article,
                 "price": new_price
             }
         ]
-        response = requests.post(url, headers=headers, json=json)
+        response = requests.post(url, headers=headers, json=json_data)
         if response.status_code == 200:
-            return f'Цена изменена, новая окончательная цена {new_price*(1-current_price_info["discount"]*0.01) * (1- current_price_info["promoCode"]*0.01)}'
-
-# print(get_info_current_price(67937389))
-# print(change_price(67937389, 2230))
+            return f'Цена изменена, новая окончательная цена {new_price*(1-current_price_info["discount"]*0.01) * (1- current_price_info["promoCode"]*0.01)} Запрос {json.dumps(json_data)}'
